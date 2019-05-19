cd6c32a532b6cfd0fc0ef3c5a9150e2860468e2d
Steven Burnett <texruska@users.noreply.github.com>
2019-05-19 13:55:40 +0000

    Add db schema

---
+++ sql/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `sites` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `URL` tinytext NOT NULL,
+  `tagID` tinytext,
+  `contentsHash` tinytext NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `slack` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `channel` tinytext NOT NULL,
+  `token` tinytext NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `slack-sites-link` (
+  `siteID` int(11) NOT NULL,
+  `slackID` int(11) NOT NULL,
+  KEY `siteID` (`siteID`),
+  KEY `slackID` (`slackID`),
+  CONSTRAINT `slack-sites-link_ibfk_1` FOREIGN KEY (`siteID`) REFERENCES `sites` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `slack-sites-link_ibfk_2` FOREIGN KEY (`slackID`) REFERENCES `slack` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
